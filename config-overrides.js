c7caf0c5c38d36a9f26496a2dee86cc13aa498e7
hewu88 <hewu88@126.com>
2020-04-17 17:36:15 +0800

    Add some folders and files

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      '@primary-color': '#419FD7' // 配置主题颜色
+    },
+  }),
+);
---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import { Button, Layout } from 'antd';
+import './App.scss';
+
+const { Header, Content, Sider } = Layout;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App h100">
+        <Layout className="h100">
+          <Header>Header</Header>
+          <Layout>
+            <Sider
+              style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0,
+              }}
+            >Sider</Sider>
+            <Content
+              style={{
+                paddingLeft:'220px'
+              }}
+            >
+              Content
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+              <h2>h100</h2>
+            </Content>
+          </Layout>
+        </Layout>
+      </div>
+    )
+  }
+}
